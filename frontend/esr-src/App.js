6494aea20bf4c4983300a82c0c30b38ca27cce5f
esru13 <esraelb313@gmail.com>
2023-05-26 18:14:08 +0300

    commit

---
+++ frontend/esr-src/App.js
@@ -0,0 +1,28 @@
+
+import React, { useState } from"react";
+import logo from './logo.svg';
+import './App.css';
+import { Login } from './Login';
+import { Registration } from './Registration';
+
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName)
+
+  }
+
+  return (
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Registration onFormSwitch={toggleForm} />
+         
+  
+      }
+      </div>
+  );
+}
+
+export default App;
