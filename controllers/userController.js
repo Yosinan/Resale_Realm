efced6db95adf1488e617c0e9f73fcfada784907
Yoseph <ymymym4634@gmail.com>
2023-06-02 10:08:40 +0300

    Resale Realm

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,7 @@ const bcrypt = require("bcrypt");
 const genToken = require("../authenticate/genToken");
 const User = require("../models/userModel");
 
+
 // Define all APIs 
 
 // Create/Register a new User
