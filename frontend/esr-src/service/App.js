cddbc74b76a34adf99fb57aa235b0193904f3f02
esru13 <esraelb313@gmail.com>
2023-06-04 22:06:48 +0300

    landing

---
+++ frontend/esr-src/service/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import Service from './Service';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Service/>
+    </div>
+  );
+}
+
+export default App;
