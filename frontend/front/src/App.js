8196cb7d17c3da91159c6a221330d7a816a130e8
Yoseph <ymymym4634@gmail.com>
2023-06-08 07:30:18 +0300

    Resale_Realm

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -1,34 +1,59 @@
 import React, { useState } from "react";
 // import { Router, Route, Switch } from "react-router";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import "./App.css";
 import "./LandingPage/App.css";
 import { Login } from "./Login";
 import { Registration } from "./Registration";
 import { Dashboard } from "./Dashboard";
+import { Landing } from "./LandingPage";
 // import { Service } from './Services/Service';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isLoginPage, setIsLoginPage] = useState(true);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoginPage, setIsLoginPage] = useState(true);
 
-  function handleLogin() {
-    setIsLoggedIn(true);
-  }
+  // function handleLogin() {
+  //   setIsLoggedIn(true);
+  // }
 
-  function toggleLoginPage() {
-    setIsLoginPage(!isLoginPage);
-  }
+  // function toggleLoginPage() {
+  //   setIsLoginPage(!isLoginPage);
+  // }
 
   return (
-    <div className="App">
-      {isLoggedIn ? (
-        <Dashboard />
-      ) : isLoginPage ? (
-        <Login onLogin={handleLogin} togglePage={toggleLoginPage} />
-      ) : (
-        <Registration togglePage={toggleLoginPage} />
-      )}
-    </div>
+    // <div className="App">
+    //   {isLoggedIn ? (
+    //     <Dashboard />
+    //   ) : isLoginPage ? (
+    //     <Login onLogin={handleLogin} togglePage={toggleLoginPage} />
+    //   ) : (
+    //     <Registration togglePage={toggleLoginPage} />
+    //   )}
+    // </div>
+
+<BrowserRouter>
+<Switch>
+  <Route exact path="/" component={Landing} />
+  <Route path="/signup" component={Registration} />
+  <Route path="/signin" component={Login} />
+</Switch>
+</BrowserRouter>
+
+/*
+<Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/signup">
+          <SignupPage toggleAuthentication={toggleAuthentication} />
+        </Route>
+        <Route path="/login">
+          <LoginPage toggleAuthentication={toggleAuthentication} />
+        </Route>
+        <Route path="/dashboard">
+          {isAuthenticated ? <DashboardPage /> : <Redirect to="/login" />}
+        </Route>
+      </Switch>
+      */
 	 
   );
 };
