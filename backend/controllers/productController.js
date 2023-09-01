4a4a1620c9827d27a374fb9504c24fca965be9b1
esru13 <esraelb313@gmail.com>
2023-09-02 01:19:12 +0300

    search

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -103,6 +103,36 @@ const editItem = async (req, res, next) => {
   }
 };
 
+ const search = async (req, res ) => {
+  try {
+    const { search, category, price } = req.query;
+
+    // Construct the query based on the provided criteria
+    const query = {};
+
+    if (search) {
+      query.name = { $regex: search, $options: 'i' }; // Case-insensitive search
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (price) {
+      const [minPrice, maxPrice] = price.split('-');
+      query.price = { $gte: parseInt(minPrice), $lte: parseInt(maxPrice) };
+    }
+
+    // Query the database
+    const products = await Product.find(query);
+
+    res.json(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
 
 module.exports = {
     getItems, 
@@ -110,4 +140,5 @@ module.exports = {
     deleteItem,
     addItem, 
     editItem, 
+    search,
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const router = express.Router();
 const app = express();
 const auth = require("../middlewares/auth");
-const {  getItems, getItemById, addItem, editItem, deleteItem } = require("../controllers/productController");
+const {  getItems, getItemById, addItem, editItem, deleteItem, search } = require("../controllers/productController");
 app.use(cors());
 // using the APIs
 router.post("/api/products/add", auth, addItem);
@@ -11,38 +11,6 @@ router.put("/api/products/edit/:id", auth, editItem);
 router.delete("/api/products/delete/:id", auth, deleteItem);
 router.get("/api/products/", getItems);
 router.get("/api/products/:id", getItemById);
-
-const Product =require('../models/productModel');
-
-router.get("/api/pro", async (req, res) => {
-    try {
-      const { search, category, price } = req.query;
-  
-      // Construct the query based on the provided criteria
-      const query = {};
-  
-      if (search) {
-        query.name = { $regex: search, $options: 'i' }; // Case-insensitive search
-      }
-  
-      if (category) {
-        query.category = category;
-      }
-  
-      if (price) {
-        const [minPrice, maxPrice] = price.split('-');
-        query.price = { $gte: parseInt(minPrice), $lte: parseInt(maxPrice) };
-      }
-  
-      // Query the database
-      const products = await Product.find(query);
-  
-      res.json(products);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Server error' });
-    }
-  });
-  
+router.get("/api/search", search);
 
 module.exports = router;
