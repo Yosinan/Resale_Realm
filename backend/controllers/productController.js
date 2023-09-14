a203564354abeaf7f28f2ecfe0f503b46a309a34
Yosinan <ymymym4634@gmail.com>
2023-09-14 20:16:36 +0300

    updates on search

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,48 +1,64 @@
 const Product = require('../models/productModel');
 const User = require("../models/userModel");
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+
+
+// Set up multer for file uploads
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './public/uploads/products');
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
 
 // Set up routes for products
 
 // Add products (logged in users only)
-const addItem = (req, res) => {
-    try {
-    const product = new Product(req.body);
-    product.save();
-    res.status(201).json({product});
-    } catch (err) {
-        res.status(500).send(err);
-    }
-};
+const addItem = (upload.single('image'), async (req, res) => {
+  try {
+    const product = new Product({
+      name: req.body.name,
+      unitPrice: req.body.unitPrice,
+      description: req.body.description,
+      images: {
+        data: req.file.buffer,  
+        contentType: req.file.mimetype,
+      },
+      category: req.body.category,
+      addedBy: req.user._id,
+    })
+    await product.save();
+    res.status(201).json({ product });
+  } catch (err) {
+    res.status(500).send("Our side" + err);
+  }
+});
 
 // GET all products
 const getItems = async (req, res, next) => {
-    try {
+  try {
     const products = await Product.find();
-     res.send(products);
-    } catch (err) {
-    next(err);
-     }
-};
-  
-// GET a product by ID
-const  getItemById = async (req, res, next) => {
-  try{
-     const product = await Product.findById(req.params.id);
-      if (product) { res.send(product); }
+    res.send(products);
   } catch (err) {
-       next(err); 
-}
+    next(err);
+  }
 };
-  
-// DELETE a product by ID (logged in users only)
-// const deleteItem = async (req, res, next) => {
-//   try{
-//       const product = await Product.findByIdAndDelete(req.params.id);
-//       if (product) { res.send("Product Deleted"); 
-//       res.status(404).send("Product not found");
-//     }
+
+// GET a product by ID
+// const getItemById = async (req, res, next) => {
+//   try {
+//     const product = await Product.findById(req.params.id);
+//     if (product) { res.send(product); }
 //   } catch (err) {
-//        next(err);
+//     next(err);
 //   }
 // };
 
@@ -64,21 +80,6 @@ const deleteItem = async (req, res, next) => {
     next(err);
   }
 };
-
-
-// EDIT a product by ID (logged in users only)
-// const editItem = async (req, res, next) => {
-//     try {
-//      const product = await Product.findByIdAndUpdate(req.params.id, req.body);
-//       res.send(product);
-//     } catch (err) {
-//       if (err) {
-//          next(err);
-//         } else if (!product) {
-//           res.status(404).send('Product not found');
-//         } 
-//       }
-// };
 
 // Edit a product by ID (logged in users and owners of the product only)
 const editItem = async (req, res, next) => {
@@ -102,10 +103,78 @@ const editItem = async (req, res, next) => {
 };
 
 
+// search products by name
+
+const searchProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find({ name: req.params.name });
+    res.send(products);
+  } catch (err) {
+    next(err);
+  }
+}
+
+// search products by category
+
+const searchProductsByCategory = async (req, res, next) => {
+  try {
+    const products = await Product.find({ category: req.params.category });
+    res.send(products);
+  } catch (err) {
+    next(err);
+  }
+}
+
+// search products by price
+
+const searchProductsByPrice = async (req, res, next) => {
+  try {
+    const products = await Product.find({ unitPrice: req.params.unitPrice });
+    res.send(products);
+  } catch (err) {
+    next(err);
+  }
+}
+
+// search 
+
+const searchAll = async (req, res) => {
+  try {
+    const { name, category, minPrice, maxPrice } = req.query;
+
+    const filter = {};
+
+    if (name) {
+      filter.name = { $regex: name, $options: 'i' };
+    }
+    if (category) {
+      filter.category = { $regex: category, $options: 'i' };
+    }
+    if (minPrice && maxPrice) {
+      filter.price = { $gte: minPrice, $lte: maxPrice };
+    } else if (minPrice) {
+      filter.price = { $gte: minPrice };
+    } else if (maxPrice) {
+      filter.price = { $lte: maxPrice };
+    }
+
+    const products = await Product.find(filter);
+
+    res.json(products);
+  } catch (err) {
+    console.error('Error fetching products:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
 module.exports = {
-    getItems, 
-    getItemById, 
-    deleteItem,
-    addItem, 
-    editItem, 
+  getItems,
+  // getItemById,
+  deleteItem,
+  addItem,
+  editItem,
+  searchProducts,
+  searchProductsByCategory,
+  searchProductsByPrice,
+  searchAll
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -3,13 +3,28 @@ const cors = require("cors");
 const router = express.Router();
 const app = express();
 const auth = require("../middlewares/auth");
-const {  getItems, getItemById, addItem, editItem, deleteItem } = require("../controllers/productController");
+const {
+    getItems,
+    // getItemById,
+    addItem,
+    editItem, 
+    deleteItem,
+    searchProductsByPrice,
+    searchProductsByCategory,
+    searchAll, 
+} = require("../controllers/productController");
+
 app.use(cors());
 // using the APIs
 router.post("/api/products/add", auth, addItem);
 router.put("/api/products/edit/:id", auth, editItem);
 router.delete("/api/products/delete/:id", auth, deleteItem);
 router.get("/api/products/", getItems);
-router.get("/api/products/:id", getItemById);
+// router.get("/api/products/:id", getItemById);
+router.get("/api/products/search/", searchAll);
+router.get("/api/products/category/:category", searchProductsByCategory);
+router.get("/api/products/price/:unitPrice", searchProductsByPrice);
+
+
 
 module.exports = router;
